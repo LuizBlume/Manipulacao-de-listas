599c0617c5d341067fedcbb552ea4b501de4f066
LuizBlume <luiz.fernando.251107@gmail.com>
2024-03-11 16:54:44 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { reactive, computed, ref } from "vue";
+const novoItem = ref("");
+
+const cidades = reactive([
+  "São Paulo",
+  "Rio de Janeiro",
+  "Belo Horizonte",
+  "Salvador",
+  "Fortaleza",
+  "Curitiba",
+  "Manaus",
+  "Recife",
+  "Porto Alegre",
+  "Brasília",
+]);
+function adicionar() {
+  cidades.push(novoItem.value);
+  novoItem.value = "";
+}
+</script>
+<template>
+  <div>
+    <p v-for="(item, index) in cidades" :key="index">{{ item }}</p>
+    <input type="text" v-model="novoItem" />
+    <button @click="adicionar">Adicionar</button>
+  </div>
+</template>
