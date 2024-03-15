579d4364f4c08e7323247adcae4e6b1ef4fd17e0
LuizBlume <luiz.fernando.251107@gmail.com>
2024-03-15 13:31:26 -0300

    NewEvent: Adição da tecla enter e estilização usando SASS.

--- src/App.vue
+++ src/App.vue
@@ -23,17 +23,29 @@ function adicionarOrdenado() {
   novoItem.value = "";
 }
 function keycode() {
-  if (event.keyCode === 13) {
-    adicionar();
-  }
+  adicionarOrdenado();
 }
 </script>
 <template>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
+    rel="stylesheet"
+  />
+
   <div class="container">
-    <link rel="stylesheet" href="./css/style.css">
-    <p class="itens" v-for="(item, index) in cidadesOrdenadas" :key="index">{{ item }}</p>
-    <input class="inpt" type="text" v-model="novoItem" />
-    <button class="button" type="submit" @click="adicionarOrdenado">Adicionar</button>
+    <link rel="stylesheet" href="./css/style.css" />
+    <p class="itens" v-for="(item, index) in cidadesOrdenadas" :key="index">
+      {{ item }}
+    </p>
+    <input
+      class="input"
+      type="text"
+      v-model="novoItem"
+      @keyup.enter="adicionarOrdenado"
+    />
+    <button class="button" type="submit">Adicionar</button>
   </div>
 </template>
 <style lang="scss" scoped>
